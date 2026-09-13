b92a094f29ec66c14f86cf99982a0d59444680ad
viniciusmacciel18-rgb <viniciusmacciel18@gmail.com>
2026-09-13 16:57:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ def cancelar(appointment_id):
     flash("Agendamento cancelado.", "ok")
     return redirect(url_for("admin"))
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
